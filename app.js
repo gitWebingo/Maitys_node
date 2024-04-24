a4614ece617a1124a7357e50853b45e0813198ea
ArnabBiswas123 <iamarnabbiswas2001@gmail.com>
2024-04-24 12:48:09 +0530

    new

--- app.js
+++ app.js
@@ -20,9 +20,31 @@ app.use('/api/v1/user',userRouter)
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
-    await app.listen(port, () => {
+    const server=await app.listen(port, () => {
       console.log(`Example app listening on port ${port}`);
     });
+    const io=require('socket.io')(server,{
+      pingTimeout:60000,
+      cors: {
+        origin: "http://localhost:5173"
+    }
+    })
+    io.on('connection',(socket)=>{
+      console.log('connected socket')
+      socket.on("setup",(vender_id)=>{
+        socket.join(vender_id)
+        socket.emit("connected");
+      })
+      socket.on('join chat',(vender_id)=>{
+        socket.join(vender_id);
+         console.log("User Joined Room: ");
+      })
+      socket.on('send_time',async())
+
+    })
+
+
+
   } catch (error) {
     console.log(error);
   }
